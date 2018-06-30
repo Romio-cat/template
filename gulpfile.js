aab639d6f798795599b917028119d16f6d3d0ccb
unknown <kaluzhenov95@gmail.com>
2018-06-30 07:47:35 +0300

    added autoreload

--- gulpfile.js
+++ gulpfile.js
@@ -2,17 +2,20 @@ var gulp 				= require('gulp'),
 		browserSync = require('browser-sync').create(),
 		sass				= require('gulp-sass');
 
-
-gulp.task('server', [reload-css], function() {
-	browserSync.init({
-      server: "./app"
-  });
-});
-
-
-gulp.task('reload-css', function() {
-	gulp.src('app/sass/**/*.sass')
+gulp.task('sass', function() {
+	gulp.src('app/sass/*.sass')
 		.pipe(sass())
 		.pipe(gulp.dest('app/css'))
 		.pipe(browserSync.stream());
 });
+
+gulp.task('server', ['sass'], function() {
+	browserSync.init({
+      server: "./app"
+  });
+  gulp.watch("app/sass/*.sass", ['sass']);
+  gulp.watch("app/*.html").on('change', browserSync.reload);
+});
+
+gulp.task('default', ['server']);
+
